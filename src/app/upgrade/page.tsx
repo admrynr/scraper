19d8070bc0d8e770c29c6127c252cf176f541667
admrynr <reyneradam@gmail.com>
2026-09-09 23:10:23 +0700

    env

--- src/app/upgrade/page.tsx
+++ src/app/upgrade/page.tsx
@@ -21,7 +21,7 @@ export default function UpgradePage() {
   useEffect(() => {
     // Load Midtrans Snap.js
     const script = document.createElement('script');
-    script.src = process.env.MIDTRANS_IS_PRODUCTION === 'true'
+    script.src = process.env.NEXT_PUBLIC_MIDTRANS_IS_PRODUCTION === 'true'
       ? 'https://app.midtrans.com/snap/snap.js'
       : 'https://app.sandbox.midtrans.com/snap/snap.js';
     script.setAttribute('data-client-key', process.env.NEXT_PUBLIC_MIDTRANS_CLIENT_KEY || '');
